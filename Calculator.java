44b810d3f83f0ae9f7f988f70d214a90464ae9b0
sethuram-1004 <132370046+sethuram-1004@users.noreply.github.com>
2023-05-04 23:57:32 +0530

    Calci-1234-Main file was uploded

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,4 @@
-class Calculator {
+public class Calculator {
     public int add(int a , int b)
     {
         return a+b;
--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        Calculator caci = new Calculator();
+        
+        System.out.println("Addition : 1,1 "+caci.add(1, 1));
 
+        System.out.println("Subraction : 1,1 "+caci.sub(1, 1));
+        System.out.println("Multiplication : 1,1 "+caci.mul(1, 1));
+    }
+}
